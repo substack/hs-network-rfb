476d050973eedd55402dfbc03b583340291b3d36
James Halliday <substack@gmail.com>
2010-01-05 14:24:27 -0900

    new WordIO class

--- word.hs
+++ word.hs
@@ -1,42 +1,35 @@
 module System.IO.Word where
 
-import System.IO
-import Data.Word
+import System.IO (Handle, hGetBuf, hPutBuf)
+import Foreign (Storable, sizeOf, mallocArray, newArray, peekArray)
 import Control.Applicative ((<$>))
-import Control.Arrow ((&&&), (***))
-import Control.Monad (replicateM)
+import Data.Word (Word8, Word16, Word32, Word64)
 
-hGetWord8 :: Handle -> IO Word8
-hGetWord8 = hGetN (8 `div` 8)
+class (Num a, Storable a) => WordIO a where
+    hGetWords :: Handle -> Int -> IO [a]
+    hGetWords = hGetWords' 0
+    
+    hGetWord :: Handle -> IO a
+    hGetWord h = head <$> hGetWords h 1
+    
+    hPutWords :: Handle -> [a] -> IO ()
+    hPutWords = hPutWords' 0
+    
+    hPutWord :: Handle -> a -> IO ()
+    hPutWord h x = hPutWords h [x]
 
-hGetWord16 :: Handle -> IO Word16
-hGetWord16 = hGetN (16 `div` 8)
+instance WordIO Word8
+instance WordIO Word16
+instance WordIO Word32
+instance WordIO Word64
 
-hGetWord32 :: Handle -> IO Word32
-hGetWord32 = hGetN (32 `div` 8)
+hGetWords' :: Storable a => a -> Handle -> Int -> IO [a]
+hGetWords' x h n = do
+    ptr <- mallocArray n
+    hGetBuf h ptr (n * sizeOf x)
+    peekArray n ptr
 
-hGetWord64 :: Handle -> IO Word64
-hGetWord64 = hGetN (64 `div` 8)
-
-hGetN :: Integral a => Int -> Handle -> IO a
-hGetN n h = foldl1 (+)
-    <$> zipWith (*) (iterate (`div` 256) (fromIntegral $ 256 ^ (n - 1)))
-    <$> replicateM n (fromIntegral . fromEnum <$> hGetChar h)
-
-hTake :: Handle -> Int -> IO String
-hTake h n = replicateM n (hGetChar h)
-
-hTakeBytes :: Handle -> Int -> IO [Int]
-hTakeBytes h n = replicateM n (fromIntegral <$> hGetWord8 h)
-
-hGetByte :: Handle -> IO Int
-hGetByte = (fromIntegral <$>) . hGetWord8
-
-hGetShort :: Handle -> IO Int
-hGetShort = (fromIntegral <$>) . hGetWord16
-
-hGetInt :: Handle -> IO Int
-hGetInt = (fromIntegral <$>) . hGetWord32
-
-hGetLong :: Handle -> IO Int
-hGetLong = (fromIntegral <$>) . hGetWord64
+hPutWords' :: Storable a => a -> Handle -> [a] -> IO ()
+hPutWords' x h xs = do
+    ptr <- newArray xs
+    hPutBuf h ptr $ (sizeOf x) * length xs
